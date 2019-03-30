f52a19c8521883e13f519694691517e5f06f2e7c
Srirupa <srirupadatta@pursuit.org>
2019-03-30 17:44:48 -0400

    imported firebase and AuthContext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { HashRouter, Route } from 'react-router-dom';
+import firebase from './firebase';
 import './App.css';
 
 // ---- Pages
 import Navbar from './components/navbar/navbar';
 
+// ---- Contexts
+import AuthContext from './contexts/auth';
 
 class App extends Component {
   render() {
