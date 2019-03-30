07f8572a37b3e7a13b592ec8f01daf05774240bc
Srirupa <srirupadatta@pursuit.org>
2019-03-30 12:35:39 -0400

    refactored into a functional component

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css'
 
-export default class Navbar extends Component {
-    render() {
-        return (
-            <>
-                <div className='navbar'>
-                    <p className="logo">lowkey</p>
-                    <div className='navLinks'>
-                        <Link to='/' className="link" >Home</Link>
-                        <Link to='/profile' className="link" >Profile</Link>
-                        <Link to='/friends' className="link" >Friends</Link>
-                        <Link to='/explore' className="link" >Explore</Link>
-                    </div>
+export default (props) => {
+    return (
+        <>
+            <div className='navbar'>
+                <p className="logo">lowkey</p>
+                <div className='navLinks'>
+                    <Link to='/home' className="link" >Home</Link>
+                    <Link to='/profile' className="link" >Profile</Link>
+                    <Link to='/friends' className="link" >Friends</Link>
+                    <Link to='/explore' className="link" >Explore</Link>
                 </div>
-            </>
-        )
-    }
+            </div>
+        </>
+    )
 }
 
 // const loggedIn and const loggedOut
