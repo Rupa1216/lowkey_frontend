19cfa379e0543435f3444637030af1bdcdf81174
Srirupa <srirupadatta@pursuit.org>
2019-03-30 11:59:46 -0400

    imported CSS and inserted Links into render

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css'
 
 export default class Navbar extends Component {
     render() {
         return (
             <>
-            <div className='navbar'>
-            
-            </div>
+                <div className='navbar'>
+                    <p className="logo">lowkey</p>
+                    <div className='navLinks'>
+                        <Link to='/' className="link" >Home</Link>
+                        <Link to='/profile' className="link" >Profile</Link>
+                        <Link to='/friends' className="link" >Friends</Link>
+                        <Link to='/explore' className="link" >Explore</Link>
+                    </div>
+                </div>
             </>
         )
     }
 }
+
+// const loggedIn and const loggedOut
