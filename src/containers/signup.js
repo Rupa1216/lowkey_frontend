42b70a987983fdb3b1b1bc754c2d32a820f9c48b
Srirupa <srirupadatta@pursuit.org>
2019-03-30 22:38:03 -0400

    imported AuthContext and exported initial Signup component

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import firebase from '../firebase';
+import AuthContext from '../contexts/auth';
+
+export default class Signup extends React.Component {
+
+    state ={
+
+    }
+
+    render() {
+
+    }
+}
