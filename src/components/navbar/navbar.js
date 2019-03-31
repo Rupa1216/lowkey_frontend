40be5e647687114e370d3aa47ef5b684817d7f9e
Srirupa <srirupadatta@pursuit.org>
2019-03-30 21:39:20 -0400

    implemented AuthContext.Consumer in the return

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,33 +6,33 @@ import './navbar.css'
 export default (props) => {
 
     const loggedOut =
-    <>
-        <li className="navItem">
-            <Link className="link" to="/signup">Sign Up</Link>
-        </li>
-        <li className="navItem">
-            <Link className="link" to="/login">Login</Link>
-        </li>
-    </>
+        <>
+            <li className="navItem">
+                <Link className="link" to="/signup">Sign Up</Link>
+            </li>
+            <li className="navItem">
+                <Link className="link" to="/login">Login</Link>
+            </li>
+        </>
 
     const loggedIn =
-    <>
-        <li className="navItem">
-            <Link to='/profile' className="link" >Profile</Link>
-        </li>
-        <li className="navItem">
-            <Link to='/notifications' className="link" >Notifications</Link>
-        </li>
-        <li className="navItem">
-            <Link to='/friends' className="link" >Friends</Link>
-        </li>
-        <li className="navItem">
-            <Link to='/explore' className="link" >Explore</Link>
-        </li>
-        <li className="navItem">
-            <Link className="link" to="/logout">Logout</Link>
-        </li>
-    </>
+        <>
+            <li className="navItem">
+                <Link to='/profile' className="link" >Profile</Link>
+            </li>
+            <li className="navItem">
+                <Link to='/notifications' className="link" >Notifications</Link>
+            </li>
+            <li className="navItem">
+                <Link to='/friends' className="link" >Friends</Link>
+            </li>
+            <li className="navItem">
+                <Link to='/explore' className="link" >Explore</Link>
+            </li>
+            <li className="navItem">
+                <Link className="link" to="/logout">Logout</Link>
+            </li>
+        </>
 
     return (
         <>
@@ -43,7 +43,14 @@ export default (props) => {
                         <li className="navItem">
                             <Link to='/home' className="link" >Home</Link>
                         </li>
-                        
+                        <AuthContext.Consumer>
+                            {
+                                user => {
+                                    if (user) return loggedIn
+                                    else return loggedOut
+                                }
+                            }
+                        </AuthContext.Consumer>
                     </div>
                 </nav>
             </div>
