66d608d8372c9d3dee839d392a328193fae7e2a2
Srirupa <srirupadatta@pursuit.org>
2019-04-02 18:30:54 -0400

    added event listener onto input tag in jsx

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -23,15 +23,15 @@ export default class Signup extends React.Component {
             <form>
             <div className="formItem">
                     <label htmlFor="exampleInputEmail1">Username</label>
-                    <input type="text" className="input" aria-describedby="emailHelp" placeholder="Enter username" name="username" />
+                    <input type="text" className="input" aria-describedby="emailHelp" placeholder="Enter username" name="username" onChange={this.handleChange} />
                 </div>
                 <div className="formItem">
                     <label htmlFor="exampleInputEmail1">Email</label>
-                    <input type="email" className="input" aria-describedby="emailHelp" placeholder="Enter email" name="email" />
+                    <input type="email" className="input" aria-describedby="emailHelp" placeholder="Enter email" name="email" onChange={this.handleChange} />
                 </div>
                 <div className="formItem">
                     <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="input" placeholder="Password" name="password" />
+                    <input type="password" className="input" placeholder="Password" name="password" onChange={this.handleChange} />
                 </div>
                 <button type="submit" className="submitButton" >Sign Up</button>
             </form>
