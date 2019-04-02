456d7aa4d6e01efed820fa809b61ba19134c32c4
Srirupa <srirupadatta@pursuit.org>
2019-04-02 17:44:55 -0400

    added name attribute into input tags

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -18,22 +18,22 @@ export default class Signup extends React.Component {
     render() {
         const { email, password, error } = this.state;
         const displayForm = <>
-            <h1>Login</h1> 
+            <h1>Sign Up</h1> 
             
             <form>
             <div className="formItem">
                     <label htmlFor="exampleInputEmail1">Username</label>
-                    <input type="text" className="input" aria-describedby="emailHelp" placeholder="Enter username" />
+                    <input type="text" className="input" aria-describedby="emailHelp" placeholder="Enter username" name="username" />
                 </div>
                 <div className="formItem">
                     <label htmlFor="exampleInputEmail1">Email</label>
-                    <input type="email" className="input" aria-describedby="emailHelp" placeholder="Enter email" />
+                    <input type="email" className="input" aria-describedby="emailHelp" placeholder="Enter email" name="email" />
                 </div>
                 <div className="formItem">
                     <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="input" placeholder="Password" />
+                    <input type="password" className="input" placeholder="Password" name="password" />
                 </div>
-                <button type="submit" className="submitButton" >Login</button>
+                <button type="submit" className="submitButton" >Sign Up</button>
             </form>
         </>;
 
