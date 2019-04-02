48e8a6e20b5dbd5f2ff3e49edad3f3cd8f84431a
Srirupa <srirupadatta@pursuit.org>
2019-04-02 00:50:29 -0400

    added state properties; inserted <form> jsx into render

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -5,22 +5,45 @@ import AuthContext from '../contexts/auth';
 export default class Signup extends React.Component {
 
     state = {
-
+        username: '',
+        email: '',
+        password: '',
+        error: ''
     }
 
     render() {
+        const displayForm = <>
+            <h1>Login</h1> 
+            
+            <form>
+            <div className="formItem">
+                    <label htmlFor="exampleInputEmail1">Username</label>
+                    <input type="text" className="input" aria-describedby="emailHelp" placeholder="Enter username" />
+                </div>
+                <div className="formItem">
+                    <label htmlFor="exampleInputEmail1">Email</label>
+                    <input type="email" className="input" aria-describedby="emailHelp" placeholder="Enter email" />
+                </div>
+                <div className="formItem">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="input" placeholder="Password" />
+                </div>
+                <button type="submit" className="submitButton" >Login</button>
+            </form>
+        </>;
+
         return (
-        <AuthContext.Consumer>
-            {
-                (user) => {
-                    if (user) {
-                        return <Redirect to='/' />
-                    } else {
-                        
+            <AuthContext.Consumer>
+                {
+                    (user) => {
+                        if (user) {
+                            return <Redirect to='/' />
+                        } else {
+                            return displayForm;
+                        }
                     }
                 }
-            }
-        </AuthContext.Consumer>
+            </AuthContext.Consumer>
         )
     }
 }
