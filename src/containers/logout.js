bfd89a92db43c7b502f440b663a4a3884bf1cff8
Srirupa <srirupadatta@pursuit.org>
2019-04-02 23:59:03 -0400

    added firebase sign out method

--- src/containers/logout.js
+++ src/containers/logout.js
@@ -3,6 +3,10 @@ import firebase from '../firebase';
 
 export default class Logout extends React.Component {
 
+    componentDidMount() {
+        firebase.auth().signOut()
+    }
+
     render() {
         return <h1>Logging out...</h1>
     }
