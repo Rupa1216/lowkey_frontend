b74d87ab55b037ecd155146e0c33531d5f917225
Srirupa <srirupadatta@pursuit.org>
2019-04-02 20:09:21 -0400

    imported Redirect, applied onClick event handler on button; inserted potential error message into render

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import firebase from '../firebase';
 import AuthContext from '../contexts/auth';
+import { Redirect } from 'react-router-dom';
 
 export default class Signup extends React.Component {
 
@@ -17,25 +18,26 @@ export default class Signup extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-    
+
         const { email, password } = this.state;
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .then((response) => {
-            console.log('Returns: ', response);
-          })
-          .catch(err => {
-            const { message } = err;
-            this.setState({ error: message });
-          })
-      }
+            .then((response) => {
+                console.log('Returns: ', response);
+            })
+            .catch(err => {
+                const { message } = err;
+                this.setState({ error: message });
+            })
+    }
 
     render() {
         const { username, email, password, error } = this.state;
+        const displayError = error === '' ? '' : <div className="alert alert-danger" role="alert">{error}</div>
         const displayForm = <>
-            <h1>Sign Up</h1> 
-            
+            <h1>Sign Up</h1>
+            {displayError}
             <form>
-            <div className="formItem">
+                <div className="formItem">
                     <label htmlFor="exampleInputEmail1">Username</label>
                     <input type="text" className="input" aria-describedby="emailHelp" placeholder="Enter username" name="username" onChange={this.handleChange} />
                 </div>
@@ -47,7 +49,7 @@ export default class Signup extends React.Component {
                     <label htmlFor="exampleInputPassword1">Password</label>
                     <input type="password" className="input" placeholder="Password" name="password" onChange={this.handleChange} />
                 </div>
-                <button type="submit" className="submitButton" >Sign Up</button>
+                <button type="submit" className="submitButton" onClick={this.handleSubmit} >Sign Up</button>
             </form>
         </>;
 
