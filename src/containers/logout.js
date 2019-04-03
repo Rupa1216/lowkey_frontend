12ad8d1eb9b25d5ab5be6b54bf9ca8a953d85d5a
Srirupa <srirupadatta@pursuit.org>
2019-04-02 23:45:44 -0400

    imported necessary modules and defined initial Logout component

---
+++ src/containers/logout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import firebase from '../firebase';
+
+export default class Logout extends React.Component {
+
+    render() {
+        return <h1>Logging out...</h1>
+    }
+}
