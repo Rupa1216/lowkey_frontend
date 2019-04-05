2ba51bdcfc0f57ffed51d56b7ced411949dc6b8d
Srirupa <srirupadatta@pursuit.org>
2019-04-04 23:11:13 -0400

    added properties to Post Component state

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -2,10 +2,18 @@ import React from 'react';
 import Moment from 'moment';
 
 export default class Post extends React.Component {
+
+    state = {
+        avatar: '',
+        username: '',
+        created_at: '',
+        content: '',
+    }
+
     render() {
         return (
             <div className="post">
-                
+
             </div>
         )
     }
