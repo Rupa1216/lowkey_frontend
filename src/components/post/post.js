6b7f3ae153ce43fc3554374be4e0e45f046c312c
Srirupa <srirupadatta@pursuit.org>
2019-04-04 22:07:40 -0400

    set up shell of Post Component

---
+++ src/components/post/post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Moment from 'moment';
+
+export default class Post extends React.Component {
+    render() {
+        return (
+            <div className="post">
+                
+            </div>
+        )
+    }
+}
