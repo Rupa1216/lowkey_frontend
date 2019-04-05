3e6ac757ad2701e7bd3245ee2ab5e1299da1e0e1
Srirupa <srirupadatta@pursuit.org>
2019-04-05 02:24:15 -0400

    imported NoPosts; added state; rendered posts list

--- src/containers/feed.js
+++ src/containers/feed.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import NoPosts from '../components/noPosts/noPosts';
 
 export default class Feed extends React.Component {
-    
+
+    state = {
+        posts: [],
+    }
+
     render() {
+
+        const { posts } = this.state;
+        const empty = <NoPosts />
+        const newsfeed = <ul>
+            {posts.map((e, i) => {
+                return <li key={i}>
+                    {e}
+                </li>
+            })}
+        </ul>
+
+
         return (
             <div className='feed'>
-            
+                {posts.length < 1 ? empty : newsfeed}
             </div>
         )
     }
