608d939e05cdb778ccabf205241ffbdb3a158eba
Srirupa <srirupadatta@pursuit.org>
2019-04-05 18:39:02 -0400

    corrected pathname of imported firebase module

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import firebase from 'firebase';
+import firebase from '../firebase';
 import AuthContext from '../contexts/auth';
 import { Redirect } from 'react-router-dom';
 
