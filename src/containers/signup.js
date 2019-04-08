ed3051a2dd31d22ea87eb528e4d3dce5727f674b
Srirupa <srirupadatta@pursuit.org>
2019-04-08 15:41:49 -0400

    importing axios; removed state properties; utilizing Context to access user data

--- src/containers/signup.js
+++ src/containers/signup.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import firebase from '../firebase';
+import axios from 'axios';
 import AuthContext from '../contexts/auth';
 import { Redirect } from 'react-router-dom';
 
 export default class Signup extends React.Component {
 
-    state = {
-        username: '',
-        email: '',
-        password: '',
-        error: ''
+    static contextType = AuthContext;
+
+    state = {   
+        error: '',
     }
 
     handleChange = (e) => {
@@ -21,8 +21,9 @@ export default class Signup extends React.Component {
 
         const { email, password } = this.state;
         firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then((response) => {
-                console.log('Returns: ', response);
+            .then((res) => {
+                console.log('Returns: ', res);
+                return axios.post('https://lowkey-sd.herokuapp.com/users/')
             })
             .catch(err => {
                 const { message } = err;
@@ -31,10 +32,11 @@ export default class Signup extends React.Component {
     }
 
     render() {
-        const { username, email, password, error } = this.state;
+        const { user_id, username, email, password, error } = this.context;
         const displayError = error === '' ? '' : <div className="alert alert-danger" role="alert">{error}</div>
         const displayForm = <>
             <h1>Sign Up</h1>
+            <br/>
             {displayError}
             <form>
                 <div className="form-group formItem">
@@ -49,7 +51,7 @@ export default class Signup extends React.Component {
                     <label htmlFor="exampleInputPassword1">Password</label>
                     <input type="password" className="form-control input" placeholder="Password" name="password" onChange={this.handleChange} value={password} />
                 </div>
-                <button type="submit" className="btn btn-primary submitButton" onClick={this.handleSubmit} >Sign Up</button>
+                <button type="submit" className="btn btn-info submitButton" onClick={this.handleSubmit} >Sign Up</button>
             </form>
         </>;
 
