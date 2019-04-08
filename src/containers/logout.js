7198dd2bd3606457b7e7fe7e4c53df58b131ee41
Srirupa <srirupadatta@pursuit.org>
2019-04-08 14:54:05 -0400

    imported Redirect and utilized it in render

--- src/containers/logout.js
+++ src/containers/logout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import firebase from '../firebase';
+import { Redirect } from 'react-router-dom'
 
 export default class Logout extends React.Component {
 
@@ -8,6 +9,6 @@ export default class Logout extends React.Component {
     }
 
     render() {
-        return <h1>Logging out...</h1>
+        return <Redirect to="/login" />
     }
 }
