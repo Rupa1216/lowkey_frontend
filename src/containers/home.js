0584de82ed8da0de2f019623fa20d304ef329901
Srirupa <srirupadatta@pursuit.org>
2019-04-08 14:52:12 -0400

    imported and inserted CreatePostButton into render

--- src/containers/home.js
+++ src/containers/home.js
@@ -3,6 +3,7 @@ import firebase from '../firebase';
 import AuthContext from '../contexts/auth';
 import { Redirect } from 'react-router-dom';
 import Feed from './feed';
+import CreatePostButton from '../components/createPostButton/createPostButton';
 
 export default class Home extends React.Component {
 
@@ -13,6 +14,7 @@ export default class Home extends React.Component {
     }
 
     render() {
+        console.log(this.context)
         return (
             <AuthContext.Consumer>
                 {
@@ -21,7 +23,9 @@ export default class Home extends React.Component {
                             return (
                                 <>
                                     <h2>Welcome back, {this.context.email}!</h2>
+                                    <p>What's on your mind?</p>
                                     <Feed />
+                                    <CreatePostButton />
                                 </>
                             )
                         } else {
