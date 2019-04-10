e911c67770dd30f3a42ca9375be9b73e9a1e134b
Srirupa <srirupadatta@pursuit.org>
2019-04-09 20:10:34 -0400

    added classNames; reorganized post layout

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import Moment from 'moment';
-import Likes from '../like/like';
+import Likes from '../likes/likes';
 
 export default (props) => {
         return (
             <div className="post">
                 <div className='post-header'>
                     <div className='post-header-left'>
-                        <img src={props.data.avatar} alt=''></img>
+                        <img src={props.data.avatar} alt='' className='rounded-circle border-info'></img>
                     </div>
                     <div className='post-header-right'>
                         <p>{props.data.username}</p>
-                        <p>{Moment(`${props.data.created_at}`, "YYYYMMDD").fromNow()}</p>
                     </div>
                 </div>
                 <div className='post-body'>
                     {props.data.content}
+                    <p>{Moment(`${props.data.created_at}`, "YYYYMMDD").fromNow()}</p>
                 </div>
                 <div className="post-footer">
                     <Likes />
