c6e0913bd5638580efda3bd54f54065a53f7d301
Srirupa <srirupadatta@pursuit.org>
2019-04-09 20:05:05 -0400

    removed icon className

--- src/components/likes/likes.js
+++ src/components/likes/likes.js
@@ -29,9 +29,9 @@ export default class Likes extends React.Component {
                 <div className='likes-container d-flex justify-content-start'>
                     <div style={{ cursor: "pointer" }}>
                         {liked === true ? (
-                            <i className="far fa-heart icon text-danger mr-3" onClick={this.handleLike} ></i>
+                            <i className="far fa-heart text-danger mr-3" onClick={this.handleLike} ></i>
                         ) : (
-                                <i className="far fa-heart icon mr-3" onClick={this.handleLike} ></i>
+                                <i className="far fa-heart mr-3" onClick={this.handleLike} ></i>
                             )}
                     </div>
                     <div className='count'>
