48578275e304232ac06297851b8e575dea038370
Srirupa <srirupadatta@pursuit.org>
2019-04-12 23:31:59 -0400

    added state properties; initial configuration of axios call to db; removed commented out code

--- src/components/createPost/createPost.js
+++ src/components/createPost/createPost.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-// import firebase from '../../firebase';
 import { Redirect } from 'react-router-dom';
 import AuthContext from '../../contexts/auth';
 import * as firebase from 'firebase';
-// import ImageService from '../services/images';
-
-//   // Initialize Firebase
-//   const config = {
-//     apiKey: "AIzaSyDnX-V1IEQDlgNv1XwraQA9jXi3pE8Yomk",
-//     authDomain: "dumb-uploader.firebaseapp.com",
-//     databaseURL: "https://dumb-uploader.firebaseio.com",
-//     projectId: "dumb-uploader",
-//     storageBucket: "dumb-uploader.appspot.com",
-//     messagingSenderId: "370924651634"
-// };
-// firebase.initializeApp(config);
 
 export default class CreatePost extends React.Component {
 
@@ -24,110 +11,79 @@ export default class CreatePost extends React.Component {
     state = {
         post: '',
         file_obj: null,
-        post_image: ''
+        post_image_name: '',
+        post_image_url: '',
+        error: '',
     }
 
     handleChange = (e) => {
         this.setState({ post: e.target.value })
     }
 
-    // handleSubmit = (e) => {
-    //     e.preventDefault();
-
-    //     const { username } = this.state;
-
-    //     axios.post('https://lowkey-sd.herokuapp.com/posts/')
-    //         .then((res) => {
-    //             console.log(res);
-    //         })
-    //         .then(() => {
-    //             const root = firebase.storage().ref();
-    //             const fileRoute = root.child(`${username}/images/${this.state.post_image}`)
-    //             return fileRoute.put(this.state.file_obj)
-    //         })
-    //         .then((snap) => {
-    //             return snap.ref.getDownloadURL()
-    //         })
-    //         .then((url) => {
-    //             console.log(url)
-    //             //send it to firebase here
-    //         })
-    //         .catch(function (err) {
-    //             console.log(err);
-    //         });
-    // }
-
     handleFileInput = (e) => {
-
-        //     this.setState({ file_obj, filename }, () => {
-        //         console.log(this.state)
-        //     })
-
-        // const file_obj = e.target.files[0];
-        //     const filename = file_obj.name
 
         const firstFile = e.target.files[0]
         const fileName = firstFile.name
 
-        console.log(firstFile.name)
         this.setState({
-            post_image: fileName,
+            post_image_name: fileName,
             file_obj: firstFile
         }, () => {
-            console.log(this.state)
+            console.log('state got set w the file', this.state)
         })
-
-        // const root = firebase.storage().ref()
-        // const newImage = root.child(file_obj.name);
-
-        // newImage.put(file_obj)
-        //     .then((snapshot) => {
-        //         return snapshot.ref.getDownloadURL()
-        //     })
-        //     .then((url) => {
-        //         console.log(url)
-        //         this.saveImage(url);
-        //     })
     }
 
     handleClick = async (e) => {
-                e.preventDefault();
 
-        console.log('context', this.context);
-        const { post_image, file_obj } = this.state
-        const { username } = this.context
+    const { post } = this.state;
+
+        e.preventDefault();
+
+        console.log('createPost context', this.context);
+        const { post_image_name, file_obj } = this.state
         const root = await firebase.storage().ref();
 
-        const fRoute = await root.child(`/images/${post_image}`)
+        const fRoute = await root.child(`/images/${post_image_name}`)
         return fRoute.put(file_obj)
             .then((snap) => {
-                console.log(snap)
                 return snap.ref.getDownloadURL()
             })
             .then((url) => {
-                console.log(url)
-                //username, email, firebase_uid , bio, profile_pic_url
-                //const { displayName, email, userId, bio} = this.state;
-                console.log(this.state)
-                // return axios.post('http://localhost:5001/users', {
-                //     username: displayName,
-                //     email: email,
-                //     firebase_uid: userId,
-                //     bio: bio,
-                //     profile_pic_url: url
-                // })
-            })
-            // .then((response) => {
-            //     console.log( 'next res', response);
-            // })
-            .catch(err => {
-                const { message } = err;
-                this.setState({
-                    err: message
+                // console.log('createPost image_url: ', url)
+                this.setState({post_image_url: url}, () => {
+                console.log('state has the url', this.state.post_image_url)
+                
+                const { fbase_uid } = this.context;
+                const { post } = this.state;
+
+                axios.post('http://localhost:3003/posts/', {
+                    fbase_uid: fbase_uid,
+                    text_content: post,
+                    image_url: url
                 })
-                console.log('yeyeyeyey', this.state)
+                })
+            })
+            .catch(error => {
+                const { message } = error;
+                this.setState({
+                    error: message
+                })
+                console.log('uh oh', this.state)
             })
     }
+
+    /* PROCESS:
+
+    user types - onChange 
+        - post content saved to state
+    user selects image - handleFileInput 
+        - fileName and file_obj saved to state
+    user clicks Submit - handleClick 
+        - get back Firebase URL of post_image_name - setState
+    ((here)) API Call creates this.state.post in posts table
+        -- generates post_id, save to state?
+
+    */
 
     render() {
         return (
@@ -138,7 +94,6 @@ export default class CreatePost extends React.Component {
                             return (
                                 <>
                                     <form>
-                                        {/* <input type='text' className='input' onChange={this.handleChange} value={this.state.post} /> */}
                                         <div className="input-group">
                                             <textarea className="form-control input" onChange={this.handleChange} value={this.state.post} aria-label="With textarea"></textarea>
                                         </div>
