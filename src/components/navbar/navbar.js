85da06d0131375fcbe4cc6c9478471ca4e945aa9
Srirupa <srirupadatta@pursuit.org>
2019-04-12 23:34:23 -0400

    imported Search module; added inline styling to Links

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../contexts/auth';
+import Search from '../search/search';
+
+
 import './navbar.css'
 
 export default (props) => {
@@ -8,29 +11,29 @@ export default (props) => {
     const loggedOut =
         <>
             <li className="navItem">
-                <Link className="link" to="/login">Login</Link>
+                <Link className="link" to="/login" style={{textDecoration: 'none'}}>Login</Link>
             </li>
             <li className="navItem">
-                <Link className="link" to="/signup">Sign Up</Link>
+                <Link className="link" to="/signup" style={{textDecoration: 'none'}}>Sign Up</Link>
             </li>
         </>
 
     const loggedIn =
         <>
             <li className="navItem">
-                <Link to='/profile' className="link" >Profile</Link>
+                <Link to='/profile' className="link" style={{textDecoration: 'none'}}>Profile</Link>
             </li>
+            {/* <li className="navItem">
+                <Link to='/notifications' className="link" style={{textDecoration: 'none'}}>Notifications</Link>
+            </li> */}
             <li className="navItem">
-                <Link to='/notifications' className="link" >Notifications</Link>
+                <Link to='/friends' className="link" style={{textDecoration: 'none'}}>Friends</Link>
             </li>
+            {/* <li className="navItem">
+                <Link to='/explore' className="link" style={{textDecoration: 'none'}}>Explore</Link>
+            </li> */}
             <li className="navItem">
-                <Link to='/friends' className="link" >Friends</Link>
-            </li>
-            <li className="navItem">
-                <Link to='/explore' className="link" >Explore</Link>
-            </li>
-            <li className="navItem">
-                <Link className="link" to="/logout">Logout</Link>
+                <Link className="link" to="/logout" style={{textDecoration: 'none'}}>Logout</Link>
             </li>
         </>
 
@@ -40,12 +43,12 @@ export default (props) => {
                 <nav className="nav">
                     <div className='allNavItems'>
                         <div>
-                            <Link to='/' className="logo">lowkey</Link>
+                            <Link to='/' className="logo" style={{textDecoration: 'none'}}>lowkey</Link>
                         </div>
-                        <div className='navLinks'>
+                        <div className='navLinks ml-2'>
                             <ul className='ul'>
                                 <li className="navItem">
-                                    <Link to='/' className="link" >Home</Link>
+                                    <Link to='/' className="link" style={{textDecoration: 'none'}}>Home</Link>
                                 </li>
                                 <AuthContext.Consumer>
                                     {
@@ -57,11 +60,12 @@ export default (props) => {
                                 </AuthContext.Consumer>
                             </ul>
                         </div>
-                        <div className="search justify-content-end">
-                        <form class="form-inline">
+                        <div className="search ml-4">
+                        {/* <form className="form-inline pb-3">
                             <input className="form-control mr-sm-2" type="search" placeholder="@Username" aria-label="Search" />
                             <button className="btn btn-sm btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-                        </form>
+                        </form> */}
+                        <Search />
                         </div>
                     </div>
                 </nav>
