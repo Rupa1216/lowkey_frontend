84482818c4b02043065bfd669f2268b5037d545e
Srirupa <srirupadatta@pursuit.org>
2019-04-12 23:51:19 -0400

    rearranged data in render

--- src/components/feedPost/feedPost.js
+++ src/components/feedPost/feedPost.js
@@ -3,24 +3,24 @@ import Moment from 'moment';
 import Likes from '../likes/likes';
 
 export default (props) => {
-        return (
-            <div className="post">
-                <div className='post-header'>
-                    <div className='post-header-left'>
-                        <img src={props.data.avatar} alt='' className='rounded-circle border-info'></img>
-                    </div>
-                    <div className='post-header-right'>
-                        <p>{props.data.username}</p>
-                    </div>
+    return (
+        <div className="post">
+            <div className='post-header'>
+                <div className='post-header-left'>
+                    <img src={props.data.avatar} alt='' className='rounded-circle border-info'></img>
                 </div>
-                <div className='post-body'>
-                    {props.data.content}
-                    <p>{Moment(`${props.data.created_at}`, "YYYYMMDD").fromNow()}</p>
-                </div>
-                <div className="post-footer">
-                    <Likes />
+                <div className='post-header-right'>
+                    <p>{props.data.username}</p>
                 </div>
             </div>
-        )
-    }
+            <div className='post-body'>
+                {props.data.content}
+                <p>{Moment(`${props.data.created_at}`, "YYYYMMDD").fromNow()}</p>
+            </div>
+            <div className="post-footer">
+                <Likes />
+            </div>
+        </div>
+    )
+}
 
